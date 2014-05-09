f2f8d28ff24c750469d807ffcc22c9294154c9fc
Liz Karaffa <lizkaraffa@gmail.com>
2014-05-09 10:36:50 -0700

    created front-page.php

---
+++ front-page.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * The main front-page template file.
+ *
+ * 
+ *
+ * @package _s
+ */
+
+get_header(); ?>
+
+	<div id="primary" class="content-area">
+		<main id="main" class="site-main" role="main">
+
+		
+
+		</main><!-- #main -->
+	</div><!-- #primary -->
+
+<?php get_sidebar(); ?>
+<?php get_footer(); ?>
