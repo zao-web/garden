5c90e3d07bd5aeb1815317c2f857125d5bdf4532
Liz Karaffa <lizkaraffa@gmail.com>
2014-05-12 14:56:18 -0700

    removed footer info about the theme

--- footer.php
+++ footer.php
@@ -12,9 +12,6 @@
 
 	<footer id="colophon" class="site-footer" role="contentinfo">
 		<div class="site-info">
-			<a href="<?php echo esc_url( __( 'http://wordpress.org/', 'garden' ) ); ?>"><?php printf( __( 'Proudly powered by %s', 'garden' ), 'WordPress' ); ?></a>
-			<span class="sep"> | </span>
-			<?php printf( __( 'Theme: %1$s by %2$s.', 'garden' ), 'garden', '<a href="http://underscores.me/" rel="designer">Underscores.me</a>' ); ?>
 		</div><!-- .site-info -->
 	</footer><!-- #colophon -->
 </div><!-- #page -->
