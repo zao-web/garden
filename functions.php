170854e433354ce71fea788f887630e770659f4b
Liz Karaffa <lizkaraffa@gmail.com>
2014-05-21 21:45:26 -0700

    removed sidebar widget. Unrelated: Entered all Staff profiles into CPT.

--- functions.php
+++ functions.php
@@ -104,16 +104,6 @@ function garden_widgets_init() {
 	) );
 
 	register_sidebar( array(
-		'name'          => __( 'Sidebar', 'garden' ),
-		'id'            => 'sidebar-1',
-		'description'   => '',
-		'before_widget' => '<aside id="%1$s" class="widget %2$s">',
-		'after_widget'  => '</aside>',
-		'before_title'  => '<h1 class="widget-title">',
-		'after_title'   => '</h1>',
-	) );
-
-	register_sidebar( array(
 		'name'          => __( 'Top-right', 'garden' ),
 		'id'            => 'top-right',
 		'description'   => '',
