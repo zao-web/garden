3a0dc9f100d3a8a8408cc346aebc2b89339d0851
Liz Karaffa <lizkaraffa@gmail.com>
2014-06-12 13:56:29 -0700

    started adding none staff post type content to staff page. Formatting help needed

--- template-staff-grid.php
+++ template-staff-grid.php
@@ -56,6 +56,12 @@ get_header(); ?>
 				
 			<?php endwhile; endif;// end of the loop. ?>
 
+			<?php while ( have_posts() ) : the_post(); ?>
+
+				<?php get_template_part( 'content', 'page' ); ?>
+
+			<?php endwhile; // end of the loop. ?>	
+
 			</div><!-- .child-content -->
 		</main><!-- #main -->
 	</div><!-- #primary -->
