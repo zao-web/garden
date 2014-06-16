ab5d3e0df48dc34ec11b8a076ff45b50ba51a906
Liz Karaffa <lizkaraffa@gmail.com>
2014-06-16 10:33:10 -0700

    created template for the blogs landing page

---
+++ template-blogs.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Template Name:  Blog Landing
+ *
+ * This is the template that creates a single section
+ * to be displayed in a Parent Sectional Page.
+ *
+ * @package garden
+ */
+
+get_header(); ?>
+
+	<div class="section-item">
+		<main id="main" class="site-main" role="main">
+
+			
+
+		</main><!-- #main -->
+	</div><!-- #primary -->
+
+<?php get_footer(); ?>
