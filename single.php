c5c8979e1096218d82c2c2d7494b844a9ce19114
Liz Karaffa <lizkaraffa@gmail.com>
2014-06-16 10:31:12 -0700

    removed featured img from single.php to avoid duplicate display

--- single.php
+++ single.php
@@ -10,14 +10,6 @@ get_header(); ?>
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main" role="main">
 
-		<header class="featured-image">	
-			<?php 
-			if ( has_post_thumbnail() ) { // check if the post has a Post Thumbnail assigned to it.
-			  the_post_thumbnail( 'full' );
-			} 
-			?>
-		</header><!-- .featured-image-->
-
 		<?php while ( have_posts() ) : the_post(); ?>
 
 			<?php get_template_part( 'content', 'single' ); ?>
