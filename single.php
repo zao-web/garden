f29f3a31762dba75ae3735f66834bda2ff98a06e
Liz Karaffa <lizkaraffa@gmail.com>
2014-10-10 16:30:16 -0700

    added infinitely looping blog post navigation

--- single.php
+++ single.php
@@ -14,9 +14,29 @@ get_header(); ?>
 
 			<?php get_template_part( 'content', 'single' ); ?>
 
-			<?php garden_post_nav(); ?>
-
 		<?php endwhile; // end of the loop. ?>
+
+			<div class="post_nav">
+			<?php 
+			/**
+			 *  Infinite next and previous post looping in WordPress
+			 */
+				if( get_adjacent_post(false, '', true) ) : ?> 
+					<p class="previous"><?php previous_post_link('%link', 'Recent Post<br> &#8592;');?></p>
+				<?php else: 
+				    $first = new WP_Query('posts_per_page=1&order=DESC'); $first->the_post();
+				    	echo '<p class="previous"><a href="' . get_permalink() . '">Recent Post<br> &#8592;</a></p>';
+				  	wp_reset_query();
+				endif; 
+				    
+				if( get_adjacent_post(false, '', false) ) : ?>
+					<p class="next"><?php next_post_link('%link', 'Next Post<br> &#8594;');?></p>
+				<?php else: 
+					$last = new WP_Query('posts_per_page=1&order=ASC'); $last->the_post();
+				    	echo '<p class="next"><a href="' . get_permalink() . '">Next Post<br> &#8594;</a></p>';
+				    wp_reset_query();
+				endif; ?>
+			</div>
 
 		</main><!-- #main -->
 	</div><!-- #primary -->
