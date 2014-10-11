1c6e985f1366ac6943245bff48dfb0b1a9887310
Liz Karaffa <lizkaraffa@gmail.com>
2014-10-10 17:16:02 -0700

    created asset and made loop only display 1 post on landing page

---
+++ home.php
@@ -0,0 +1,44 @@
+<?php
+/**
+ * The Template for displaying all single posts.
+ *
+ * @package _s
+ */
+
+get_header(); ?>
+
+	<div id="primary" class="content-area">
+		<main id="main" class="site-main" role="main">
+
+		<?php $i = 1; while ( have_posts() && $i < 2 ) : the_post(); ?>
+
+			<?php get_template_part( 'content', 'single' ); ?>
+
+		<?php $i++; endwhile; // end of the loop. ?>
+
+			<div class="post_nav">
+			<?php 
+			/**
+			 *  Infinite next and previous post looping in WordPress
+			 */
+				if( get_adjacent_post(false, '', true) ) : ?> 
+					<p class="previous"><?php previous_post_link('%link', 'Recent Post<br> &#8592;');?></p>
+				<?php else: 
+				    $first = new WP_Query('posts_per_page=1&order=DESC'); $first->the_post();
+				    	echo '<p class="previous"><a href="' . get_permalink() . '">Recent Post<br> &#8592;</a></p>';
+				  	wp_reset_query();
+				endif; 
+				    
+				if( get_adjacent_post(false, '', false) ) : ?>
+					<p class="next"><?php next_post_link('%link', 'Next Post<br> &#8594;');?></p>
+				<?php else: 
+					$last = new WP_Query('posts_per_page=1&order=ASC'); $last->the_post();
+				    	echo '<p class="next"><a href="' . get_permalink() . '">Next Post<br> &#8594;</a></p>';
+				    wp_reset_query();
+				endif; ?>
+			</div>
+
+		</main><!-- #main -->
+	</div><!-- #primary -->
+
+<?php get_footer(); ?>
